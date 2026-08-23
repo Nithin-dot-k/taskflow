d7c40e73f74396ec63040ac5a3920cebc531414e
nithin <nithinjr76@gmail.com>
2026-08-23 20:02:48 +0530

    feat: implement environment-based configuration using pydantic-settings

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from config import settings
 
-app=FastAPI(title="TaskFlow API")
+app=FastAPI(title=settings.PROJECT_NAME)
 
 @app.get("/")
 def root_read():
-    return {"message":"Welcome to the TaskFlow application!"}
+    return {"message":f"Welcome to the {settings.PROJECT_NAME}!"}
 
